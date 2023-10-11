0253107e83e05412c0c12f46a7f08210c4e25432
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-10-12 05:16:58 +0700

    Handling GET / & /new route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    id: 0,
+    user: "Amando",
+    message: "Hi there!",
+    createdAt: new Date()
+  },
+  {
+    id: 1,
+    user: "Charles",
+    message: "What a beautiful world!",
+    createdAt: new Date()
+  }
+];
+
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages });
 });
+
+router.route('/new')
+  .get(function (req, res) {
+    res.render('new', { title: 'Express' });
+  })
+  .post(function (req, res) {
+    res.send('successfully added message')
+  })
 
 module.exports = router;
